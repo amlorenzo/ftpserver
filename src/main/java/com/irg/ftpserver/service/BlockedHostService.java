6bf6b51bda8395c94997ae0b778edd4633d928fa
alex lorenzo <alorenzo@trellushealth.com>
2024-07-16 11:42:59 +0200

    Went ahead and fixed logging output to include a reason for failure, also validated by testing: tested with valid user and invalid password, invalid user and valid password, valid user and invalid key, invalid user and invalid key. Will work on some documentation today and commit that, afterwards branching will begin as code in current state is a functional FTP server. Will work on max_idle some other time.

--- src/main/java/com/irg/ftpserver/service/BlockedHostService.java
+++ src/main/java/com/irg/ftpserver/service/BlockedHostService.java
@@ -78,8 +78,8 @@ public class BlockedHostService implements ApplicationEventPublisherAware {
                                         ServerSession session) {
         int attemptCount = userLookupAttempts.getOrDefault(ipAddress, 0) + 1;
         userLookupAttempts.put(ipAddress, attemptCount);
-        logger.info("{} - Recorded failed user lookup attempt from IP Address: {}, Attempt count: {}",
-                callingMethod, ipAddress, attemptCount);
+        logger.info("{} - Recorded failed user lookup attempt from IP Address: {}, Attempt count: {} Reason {}",
+                callingMethod, ipAddress, attemptCount, reason);
 
         if (attemptCount >= maxLoginAttempts) {
             blockHost(ipAddress, reason, callingMethod, session);
@@ -101,8 +101,8 @@ public class BlockedHostService implements ApplicationEventPublisherAware {
                                             ServerSession session) {
         int attemptCount = passwordAttempts.getOrDefault(ipAddress, 0) + 1;
         passwordAttempts.put(ipAddress, attemptCount);
-        logger.info("{} - Recorded failed password attempt from IP Address: {}, Attempt count: {}",
-                callingMethod, ipAddress, attemptCount);
+        logger.info("{} - Recorded failed password attempt from IP Address: {}, Attempt count: {} Reason: {}",
+                callingMethod, ipAddress, attemptCount, reason);
 
         if (attemptCount >= maxLoginAttempts) {
             blockHost(ipAddress, reason, callingMethod, session);
@@ -121,8 +121,8 @@ public class BlockedHostService implements ApplicationEventPublisherAware {
      * @param session       the server session
      */
     private void blockHost(String ipAddress, String reason, String callingMethod, ServerSession session) {
-        logger.warn("{} - Blocking IP Address: {} due to exceeded maximum login attempts: {}",
-                callingMethod, ipAddress, maxLoginAttempts);
+        logger.warn("{} - Blocking IP Address: {} due to exceeded maximum login attempts: {}, Reason: {}",
+                callingMethod, ipAddress, maxLoginAttempts, reason);
         userLookupAttempts.remove(ipAddress);
         passwordAttempts.remove(ipAddress);
 
